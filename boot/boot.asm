588a116dad5ea5a1e363fe9d92a7b0d8e0852283
Carlos Mares <microsystemags@hotmail.com>
2020-09-30 05:04:56 -0500

    Eliminacion de ibrerias Repetidas, comienzo de la Documentacion del Codigo

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,10 +1,10 @@
-[BITS 32]
+[BITS 32] ; Se especifica que es de tipo x86 o de 32 bits
 
-[global start]
-[extern k_x_main] ; this is in the c file
+[global start] ; Etiqueta que apunta al binario de inicio del programa
+[extern k_x_main] ; esto esta en el archivo C de Kernel
 
 start:
-  call k_x_main
+  call k_x_main ;Manda llamar a la funcion principal del kernel
 
-  cli  ; stop interrupts
+  cli  ; Detener Interrupciones
   hlt ; halt the CPU
--- include/sys/sys.h
+++ include/sys/sys.h
@@ -1,5 +1,7 @@
 #ifndef SYS_H
 #define SYS_H
-extern void resboot();
+//Se define el metodo de reinicio del sistema 
+extern void reboot();
+// Se define el metodo de apagado del sistema
 extern void shutdown();
 #endif // !SYS_H
