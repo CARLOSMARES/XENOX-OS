deb5fbae3751cb3b71f8293bece0e2421bd8a617
CarlosMares <microsystemags@hotmail.com>
2020-12-31 03:15:23 -0600

    Se Codifica la libreria io.h

--- include/asm/io.h
+++ include/asm/io.h
@@ -1,4 +1,28 @@
 #ifndef _IO_H
 #define _IO_H
 
+#define outb(value,port)\
+ __asm__("outb %%a1,%%dx"::"a" (value),"d"(port));
+
+#define inb(port) ({ \
+unsigned char _v; \
+__asm__ volatile ("inb %%dx,%%al":"=a" (_v):"d" (port)); \
+_v; \
+})
+
+#define outb_p(value,port) \
+__asm__ ("outb %%al,%%dx\n" \
+		"\tjmp 1f\n" \
+		"1:\tjmp 1f\n" \
+		"1:"::"a" (value),"d" (port))
+
+#define inb_p(port) ({ \
+unsigned char _v; \
+__asm__ volatile ("inb %%dx,%%al\n" \
+	"\tjmp 1f\n" \
+	"1:\tjmp 1f\n" \
+	"1:":"=a" (_v):"d" (port)); \
+_v; \
+})
+
 #endif
