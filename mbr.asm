cb072d64d37affd10dfac8875fb82ee000ddd292
Carlos Mares <microsystemags@hotmail.com>
2023-03-06 16:08:27 -0600

    se crea el bootloader en mbr.asm

---
+++ mbr.asm
@@ -0,0 +1,25 @@
+[bits 16]
+[org 0x7c00]
+KERNEL_OFFSET equ 0x1000
+mov [BOOT_DRIVE], dl
+mov bp, 0x9000
+mov sp, bp
+call load_kernel
+call switch_to_32bit
+jmp $
+
+%include "disk.asm"
+%include "gdt.asm"
+%include "switch-to-32bit.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
+
+BOOT_DRIVE db 0
+times 510 - ($ - $$) db 0
+dw 0xaa55
